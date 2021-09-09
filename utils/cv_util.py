fdc3cae9fc9a814fcdf986e84b53d2b5e9e6063c
xyqyear <215908558@qq.com>
2021-09-09 22:55:43 +0800

    add source for find_image_location function

--- utils/cv_util.py
+++ utils/cv_util.py
@@ -10,6 +10,9 @@ def image2array(im: Image.Image) -> np.ndarray:
 def find_image_location(
     image: np.ndarray, template: np.ndarray
 ) -> tuple[float, np.ndarray]:
+    """
+    this function is modified from https://stackoverflow.com/questions/65945434/template-matching-with-python-3-9-1-opencv-python-4-5-1-48-and-mask-transparen
+    """
     alpha_channel = np.array(cv2.split(template)[3])
     result = cv2.matchTemplate(
         image, template, cv2.TM_CCOEFF_NORMED, mask=alpha_channel
